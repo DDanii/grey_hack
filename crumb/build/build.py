5be1c3130e144e3b55f2b9b7c4ac5d3efb57e14e
robb83 <robb83@gmail.com>
2023-07-24 10:25:05 +0200

    crumb: simple hacking toolkit with server support (exploit db)

---
+++ crumb/build/build.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import re
+
+source_directory = os.path.join(os.path.dirname(__file__), "..")
+
+sources = ["server_shared.src", "server_install.src", "server_decrypt.src", "server_scan.src", "server_main.src"]
+
+contents = ""
+lines = []
+for file in sources:
+	with open( os.path.join(source_directory, file), "rt") as f:
+		contents = contents + "\n" + f.read()
+
+print(contents)
